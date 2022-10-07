1d9e3fe3cf1658238ba1473072b1c7cfdb4c6c32
Sqaarf <mrscarf19@gmail.com>
2022-10-07 21:08:21 +0200

    (feat): hello-cargo project added

---
+++ hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa.d
@@ -0,0 +1,5 @@
+/home/sqaarf/Documents/PROG/RUST/hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa: src/main.rs
+
+/home/sqaarf/Documents/PROG/RUST/hello-cargo/target/debug/deps/hello_cargo-9c51a689618826fa.d: src/main.rs
+
+src/main.rs:
---
+++ hello-cargo/target/debug/hello-cargo.d
@@ -0,0 +1,1 @@
+/home/sqaarf/Documents/PROG/RUST/hello-cargo/target/debug/hello-cargo: /home/sqaarf/Documents/PROG/RUST/hello-cargo/src/main.rs
